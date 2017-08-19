b0821f67a071015f22cb7beec4f99b89502d9fb6
Chang Shu-huai <junsuwhy@gmail.com>
2017-08-19 13:05:19 +0800

    Modify to mobile friendly.

--- js/script.js
+++ js/script.js
@@ -43,11 +43,11 @@
             console.log()
             
             if(maxAns == 1){
-                str = '意志力的大富翁啊！<br>人有兩隻腳，大家都知道，但錢有四隻腳，我就追不到。<br>每天早上還來不及睜開眼睛就開始跟時間賽跑，何時能停下來好好休息呢？<div><img src="img/result1.jpg"/></div>';
+                str = '意志力的大富翁啊！<br>人有兩隻腳，大家都知道，但錢有四隻腳，我就追不到。<br>每天早上還來不及睜開眼睛就開始跟時間賽跑，何時能停下來好好休息呢？<div><a onclick="Reveal.right();" href="javascript:void(0);"><img src="img/result1.jpg"/></a></div>';
             }else if(maxAns == 2){
-                str = '明日的大富翁啊！<br>「去年給自己定了目標存款三萬，今天掐一下手指還差五萬」<br>現在還買不起房子，現在還沒有存款，<br>沒關係，人人都有機會，在夢裡變成大富翁！<div><img src="img/result2.jpg"/></div>';
+                str = '明日的大富翁啊！<br>「去年給自己定了目標存款三萬，今天掐一下手指還差五萬」<br>現在還買不起房子，現在還沒有存款，<br>沒關係，人人都有機會，在夢裡變成大富翁！<div><a onclick="Reveal.right();" href="javascript:void(0);"><img src="img/result2.jpg"/></a></div>';
             }else if(maxAns == 3){
-                str = '心靈的大富翁啊！<br>只要是能用錢解決的事，一件都解決不了呢<div><img src="img/result3.jpg"/></div>';
+                str = '心靈的大富翁啊！<br>只要是能用錢解決的事，一件都解決不了呢<div><a onclick="Reveal.right();" href="javascript:void(0);"><img src="img/result3.jpg"/></a></div>';
                      }
             console.log(str);
             setFinalAnswer(str);
